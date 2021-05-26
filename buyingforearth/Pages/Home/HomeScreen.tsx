5080ef5b88f3fc8d2965cb94958076da43433b39
JunSeobKim <wnstjq616@gmail.com>
2021-05-26 21:24:02 +0900

    Add - test carousel

--- buyingforearth/Pages/Home/HomeScreen.tsx
+++ buyingforearth/Pages/Home/HomeScreen.tsx
@@ -1,11 +1,66 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Dimensions, Text, View} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
+type renderitem = {
+  title: string;
+  text: string;
+};
+
+const {width: viewportWidth} = Dimensions.get('window');
 function HomeScreen() {
+  let data = [
+    {
+      title: 'Item 1',
+      text: 'Text 1',
+    },
+    {
+      title: 'Item 2',
+      text: 'Text 2',
+    },
+    {
+      title: 'Item 3',
+      text: 'Text 3',
+    },
+    {
+      title: 'Item 4',
+      text: 'Text 4',
+    },
+    {
+      title: 'Item 5',
+      text: 'Text 5',
+    },
+  ];
+
+  let _renderItem = ({item, index}: {item: renderitem; index: number}) => {
+    return (
+      <View
+        style={{
+          backgroundColor: 'floralwhite',
+          borderRadius: 5,
+          height: 250,
+          padding: 50,
+          marginLeft: 25,
+          marginRight: 25,
+        }}>
+        <Text style={{fontSize: 30}}>{item.title}</Text>
+        <Text>{item.text}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
+      <Carousel
+        data={data}
+        renderItem={_renderItem}
+        sliderWidth={viewportWidth}
+        itemWidth={viewportWidth}
+        slideStyle={{width: viewportWidth}}
+        inactiveSlideOpacity={1}
+        inactiveSlideScale={1}
+      />
     </View>
   );
 }
