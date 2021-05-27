641c6999573fa85bd2296681be67a947257ba7ad
JunSeobKim <wnstjq616@gmail.com>
2021-05-27 20:15:35 +0900

    Completed - home structure and ActivityIndicator

--- buyingforearth/Pages/Home/HomeScreen.tsx
+++ buyingforearth/Pages/Home/HomeScreen.tsx
@@ -1,7 +1,13 @@
-import {StackScreenProps} from '@react-navigation/stack';
-import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {Alert, Button, Dimensions, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Dimensions,
+  Image,
+  Text,
+  View,
+} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import Carousel from 'react-native-snap-carousel';
 import {fetchHome} from '../../Helper/fetchApi';
 
@@ -18,14 +24,10 @@ interface HomeProduct {
   order_num: number;
 }
 
-type renderitem = {
-  title: string;
-  text: string;
-};
-
 const {width: viewportWidth} = Dimensions.get('window');
 
 function HomeScreen() {
+  const [isLoading, setIsLoading] = useState(true);
   const [homeProduct, setHomeProduct] = useState([
     {
       name: '없음',
@@ -35,15 +37,16 @@ function HomeScreen() {
   ]);
 
   useEffect(() => {
+    setIsLoading(true);
     let iniHome = async () => {
       try {
         await fetchHome()
           .then(res => {
             setHomeProduct(res.data.list);
-            console.log('ddd : ', homeProduct);
           })
+          .then(() => setIsLoading(false))
           .catch(error => {
-            console.log(error); //cors error
+            console.log(error); //ios : http error
           });
       } catch (error) {
         Alert.alert('네트워크 에러', '상품 정보를 가져오지 못했어요.', [
@@ -60,57 +63,59 @@ function HomeScreen() {
     iniHome();
   }, []);
 
-  let data = [
-    {
-      title: 'Item one',
-      text: 'Text 1',
-    },
-    {
-      title: 'Item 2',
-      text: 'Text 2',
-    },
-    {
-      title: 'Item 3',
-      text: 'Text 3',
-    },
-    {
-      title: 'Item 4',
-      text: 'Text 4',
-    },
-    {
-      title: 'Item 5',
-      text: 'Text 5',
-    },
-  ];
-
-  let _renderItem = ({item, index}: {item: HomeProduct; index: number}) => {
+  let _renderItem = ({item, index}: {item: Product; index: number}) => {
     return (
       <View
         style={{
           backgroundColor: 'floralwhite',
-          borderRadius: 5,
           height: 250,
-          padding: 50,
-          marginLeft: 25,
-          marginRight: 25,
         }}>
-        <Text style={{fontSize: 30}}>{item.name}</Text>
+        <Image
+          source={{uri: item.thumbnail}}
+          style={{width: '100%', height: 250}}
+        />
       </View>
     );
   };
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Carousel
-        data={homeProduct.find(e => e.order_num === 1)}
-        renderItem={_renderItem}
-        sliderWidth={viewportWidth}
-        itemWidth={viewportWidth}
-        slideStyle={{width: viewportWidth}}
-        inactiveSlideOpacity={1}
-        inactiveSlideScale={1}
-      />
-    </View>
+    <ScrollView>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#34CDAB" />
+      ) : (
+        <>
+          <Text>{homeProduct[0].name}</Text>
+          <Carousel
+            data={homeProduct[0].products}
+            renderItem={_renderItem}
+            sliderWidth={viewportWidth}
+            itemWidth={viewportWidth}
+            slideStyle={{width: viewportWidth}}
+            inactiveSlideOpacity={1}
+            inactiveSlideScale={1}
+          />
+          {/* TODO: change scrollview */}
+          <Text>{homeProduct[1].name}</Text>
+          <Carousel
+            data={homeProduct[1].products}
+            renderItem={_renderItem}
+            sliderWidth={viewportWidth}
+            itemWidth={200}
+            inactiveSlideOpacity={1}
+            inactiveSlideScale={1}
+          />
+          <Text>{homeProduct[2].name}</Text>
+          <Carousel
+            data={homeProduct[2].products}
+            renderItem={_renderItem}
+            sliderWidth={viewportWidth}
+            itemWidth={200}
+            inactiveSlideOpacity={1}
+            inactiveSlideScale={1}
+          />
+        </>
+      )}
+    </ScrollView>
   );
 }
 
