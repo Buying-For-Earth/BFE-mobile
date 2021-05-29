8d2791497c7c5d33997a843f24908ed4c62f1056
JunSeobKim <wnstjq616@gmail.com>
2021-05-30 00:50:58 +0900

    Add - bottom tab icons

--- buyingforearth/App.tsx
+++ buyingforearth/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
 import HomeStackScreen from './Pages/Home';
 import SearchScreen from './Pages/Search';
 import CategoryScreen from './Pages/Category';
@@ -17,12 +16,82 @@ const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={{flex: 1}}>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeStackScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Category" component={CategoryScreen} />
-          <Tab.Screen name="Mypage" component={MypageScreen} />
-          <Tab.Screen name="Cart" component={CartScreen} />
+        <Tab.Navigator tabBarOptions={{showLabel: false}}>
+          <Tab.Screen
+            name="Home"
+            component={HomeStackScreen}
+            options={{
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Icon
+                    name={focused ? 'ios-home' : 'ios-home-outline'}
+                    size={25}
+                    color="#34CDAB"
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Icon
+                    name={focused ? 'ios-search' : 'ios-search-outline'}
+                    size={25}
+                    color="#34CDAB"
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Category"
+            component={CategoryScreen}
+            options={{
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Icon
+                    name={focused ? 'ios-menu' : 'ios-menu-outline'}
+                    size={25}
+                    color="#34CDAB"
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Mypage"
+            component={MypageScreen}
+            options={{
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Icon
+                    name={focused ? 'ios-person' : 'ios-person-outline'}
+                    size={25}
+                    color="#34CDAB"
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Icon
+                    name={focused ? 'ios-cart' : 'ios-cart-outline'}
+                    size={25}
+                    color="#34CDAB"
+                  />
+                );
+              },
+            }}
+          />
         </Tab.Navigator>
       </SafeAreaView>
     </NavigationContainer>
